7b5e378801c0db1c8c07d953b0f086d37a4499f2
Jaime <3794310+AhiyaHiya@users.noreply.github.com>
2023-11-21 11:30:46 -0700

    Update types.h, correct build on Linux

--- source/include/image/types.h
+++ source/include/image/types.h
@@ -5,11 +5,12 @@
 
 #pragma once
 
+#include <cstdint>
 #include <vector>
 
-using pixels_wide_t = size_t;
-using pixels_high_t = size_t;
-using point_t = uint32_t;
+using pixels_wide_t = std::uint32_t;
+using pixels_high_t = std::uint32_t;
+using point_t = std::uint32_t;
 
 // compilers love 1d vec's for vectorization
 template <typename T>
