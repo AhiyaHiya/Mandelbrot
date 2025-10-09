4307cab62682814d3e48d164376d5874d48f0f87
Jaime O. Rios <3794310+AhiyaHiya@users.noreply.github.com>
2025-10-09 16:08:31 -0700

    Move typedefs out of class

--- MandelbrotSet/Include/MandelbrotSet.hpp
+++ MandelbrotSet/Include/MandelbrotSet.hpp
@@ -9,24 +9,28 @@
 template <typename T>
 using vec_1d = std::vector<T>;
 
+using red_t = std::int32_t;
+using green_t = std::int32_t;
+using blue_t = std::int32_t;
+using alpha_t = std::int32_t;
+
+using rgba_t = std::tuple<red_t, green_t, blue_t, alpha_t>;
+
 class MandelbrotSet
 {
-    using red_t = std::int32_t;
-    using green_t = std::int32_t;
-    using blue_t = std::int32_t;
-    using alpha_t = std::int32_t;
+    
 public:
 MandelbrotSet()
     : m_maxChannelValue{std::numeric_limits<std::int32_t>::max()},
         m_maxIterations{m_maxChannelValue} {}
 
-auto PixelAtCoordinate(const double scaled_x, const double scaled_y) -> std::tuple<red_t, green_t, blue_t, alpha_t>;
-auto PixelAtIteration(const uint32_t iteration) -> std::tuple<red_t, green_t, blue_t, alpha_t>;
+auto PixelAtCoordinate(const double scaled_x, const double scaled_y) -> rgba_t;
+auto PixelAtIteration(const uint32_t iteration) -> rgba_t;
 
 private:
     const std::int32_t m_maxChannelValue;
     const std::int32_t m_maxIterations;
-}
+};
 
 auto CreateMandelbrotImage(const double x_max, const double x_min, const double y_max, const double y_min,
                            const size_t width, const size_t height) -> vec_1d<std::uint32_t>;
