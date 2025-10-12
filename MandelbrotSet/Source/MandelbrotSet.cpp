7fa00a5dbc2af641a30b96eab614aee7b0dac139
Jaime O. Rios <3794310+AhiyaHiya@users.noreply.github.com>
2025-10-12 16:53:24 -0700

    Experiment with mdspan

--- MandelbrotSet/Source/MandelbrotSet.cpp
+++ MandelbrotSet/Source/MandelbrotSet.cpp
@@ -1,6 +1,11 @@
 
 #include "MandelbrotSet.hpp"
 
+#include <cmath>
+#if (0)
+#include <mdspan>
+#endif
+
 auto MandelbrotSet::PixelAtCoordinate(const double scaled_x, const double scaled_y) -> std::tuple<red_t, green_t, blue_t, alpha_t>
 {
     double x0 = 0.0;
@@ -48,28 +53,44 @@ auto MandelbrotSet::PixelAtIteration(const uint32_t iteration) -> std::tuple<red
     return std::make_tuple(0, 0, 0, 0);
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename T>
+auto scale_coordinate(const T coordinate, const T min, const T max, const auto size) -> T
+{
+    return coordinate * ((max - min) / size) + min;
+}
+
 auto CreateMandelbrotImage(const double x_max, const double x_min, const double y_max, const double y_min,
-                             const size_t width, const size_t height) -> vec_1d<std::uint32_t>
+                           const pixels_wide_t width, const pixels_high_t height) -> vec_1d<std::uint32_t>
 {
     auto mandelbrotSet = MandelbrotSet();
 
     constexpr auto channelCount = 4u; // RGBA
-    auto image = std::vector<std::uint32_t>(width * height * channelCount);
+    auto           image        = std::vector<std::uint32_t>(width * height * channelCount);
 
+#if (0)
+    auto spannedImage = std::mdspan(image.data(), height, width, channelCount);
+#endif
     for (auto y = 0; y < height; ++y)
     {
         for (auto x = 0; x < width; ++x)
         {
-            const auto scaled_x = mandelbrotSet.scale_coordinate(static_cast<double>(x), x_min, x_max, width);
-            const auto scaled_y = mandelbrotSet.scale_coordinate(static_cast<double>(y), y_min, y_max, height);
+            const auto scaled_x = scale_coordinate(static_cast<double>(x), x_min, x_max, width);
+            const auto scaled_y = scale_coordinate(static_cast<double>(y), y_min, y_max, height);
 
             const auto [red, green, blue, alpha] = mandelbrotSet.PixelAtCoordinate(scaled_x, scaled_y);
-
-            const auto offset = position::offset_in_interleaved_1d_vec(4, width, x, y);
+#if (1)
+            const auto offset = position::offset_in_interleaved_1d_vec(width, x, y, channelCount);
             image[offset]     = static_cast<std::uint32_t>(red);
             image[offset + 1] = static_cast<std::uint32_t>(green);
             image[offset + 2] = static_cast<std::uint32_t>(blue);
             image[offset + 3] = static_cast<std::uint32_t>(alpha);
+#else
+            spannedImage[x, y, 0] = static_cast<std::uint32_t>(red);
+            spannedImage[x, y, 1] = static_cast<std::uint32_t>(green);
+            spannedImage[x, y, 2] = static_cast<std::uint32_t>(blue);
+            spannedImage[x, y, 3] = static_cast<std::uint32_t>(alpha);
+#endif
         }
     }
 
