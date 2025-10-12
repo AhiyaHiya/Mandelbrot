cd1c58d466134d208504c8bf8a0cce4a552b3366
Jaime O. Rios <3794310+AhiyaHiya@users.noreply.github.com>
2025-10-12 16:53:35 -0700

    Add missing template functions

--- MandelbrotSet/Include/MandelbrotSet.hpp
+++ MandelbrotSet/Include/MandelbrotSet.hpp
@@ -16,6 +16,35 @@ using alpha_t = std::int32_t;
 
 using rgba_t = std::tuple<red_t, green_t, blue_t, alpha_t>;
 
+using pixels_wide_t = std::uint32_t;
+using pixels_high_t = std::uint32_t;
+using point_t = std::uint32_t;
+
+namespace position
+{
+/**
+ For use with 1d_vec that is storing pixels in interleaved format: e.g. rgbrgbrgb
+ Will return memory offset within 1d_vec for x,y coordinate and desired channel.
+ */
+template <const uint32_t channel_count>
+auto offset_in_interleaved_1d_vec(const pixels_wide_t width, const point_t x, const point_t y, const uint32_t channel) -> size_t
+{
+  const auto offset = (y * width + x) * channel_count + channel;
+  return offset;
+}
+
+/**
+ For use with 1d_vec that is storing pixels in planar format: e.g. rrrgggbbb
+ Will return memory offset within 1d_vec for x,y coordinate and desired channel.
+ */
+template <const pixels_wide_t width, const pixels_wide_t height>
+auto offset_in_planar_1d_vec(const point_t x, const point_t y, const uint32_t channel) -> size_t
+{
+  const auto offset = (channel * (width * height)) + (y * width + x);
+  return offset;
+}
+} // namespace position
+
 class MandelbrotSet
 {
     
@@ -33,4 +62,4 @@ private:
 };
 
 auto CreateMandelbrotImage(const double x_max, const double x_min, const double y_max, const double y_min,
-                           const size_t width, const size_t height) -> vec_1d<std::uint32_t>;
+                           const pixels_wide_t width, const pixels_high_t height) -> vec_1d<std::uint32_t>;
